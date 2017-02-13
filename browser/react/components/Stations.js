5863ca03a80e2bfd8980f11b3763c92102eed095
Jason Lam <jaylam111@gmail.com>
2017-02-13 12:31:21 -0500

    init

---
+++ browser/react/components/Stations.js
@@ -0,0 +1,22 @@
+
+
+const DUMMY_STATIONS_DATA = [
+  { name: '90s Hip Hop' },
+  { name: 'Death Metal' },
+  { name: 'Classical' }
+];
+
+<div>
+  <h3>Stations</h3>
+  <div className="list-group">
+    {
+      DUMMY_STATIONS_DATA.map(station => {
+        return (
+          <div className="list-group-item" key={station.name}>
+            <Link to={'fill/me/in/later'}>{station.name}</Link>
+          </div>
+        );
+      })
+    }
+  </div>
+</div>
